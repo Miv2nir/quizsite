68b98652391a57dfea2dc6bfbc40c06b00f19a2a
miv2nir <miv2nir@gmail.com>
2024-05-13 19:37:40 +0000

    dynamic grid implementation

--- quizsite/backend/static/script.js
+++ quizsite/backend/static/script.js
@@ -7,10 +7,13 @@
 //var sidebar = document.getElementsByClassName('sidebar')[0];
 function width_update() {
     var assignments_list = document.getElementsByClassName('assignments-list')[0];
+    var assignments_grid = document.getElementsByClassName('assignments-grid')[0];
     var sidebar = document.getElementsByClassName('sidebar')[0];
     var precalc = Math.min(((window.innerWidth - sidebar.clientWidth - 40) / 20), 63);
     var target_width = precalc.toString() + "rem";
+    var target_width_2 = (precalc + 0.9).toString() + "rem";
     assignments_list.style.maxWidth = target_width;
+    assignments_grid.style.maxWidth = target_width_2;
     console.log(target_width, assignments_list.clientWidth);
     requestAnimationFrame(width_update);
 }
