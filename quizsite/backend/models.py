0efaa700400eb153b6f5610bd2938b3b0772c5f2
miv2nir <miv2nir@gmail.com>
2024-05-20 19:05:44 +0000

    course model

--- quizsite/backend/models.py
+++ quizsite/backend/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
 # Create your models here.
+class Course(models.Model):
+    name=models.CharField(max_length=30)
+    author=models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.name
