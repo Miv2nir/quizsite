ff8d506cf6f6114257e4f5d3f9617026d6c517f8
miv2nir <miv2nir@gmail.com>
2024-05-20 19:18:29 +0000

    access level setting

--- quizsite/backend/models.py
+++ quizsite/backend/models.py
@@ -6,6 +6,12 @@ from django.contrib.auth.models import User
 class Course(models.Model):
     name=models.CharField(max_length=30)
     author=models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    ACCESS_LEVEL = (
+        ('A','Public'),
+        ('B','Unlisted'),
+        ('C','Private'),
+    )
+    access=models.CharField(max_length=1,choices=ACCESS_LEVEL,default='C')
 
     def __str__(self):
         return self.name
