421724b028edf2a3e0cf1637f094448fdc624bed
miv2nir <miv2nir@gmail.com>
2024-05-23 13:20:34 +0000

    markdown renderer on pages

---
+++ quizsite/backend/static/mdread.js
@@ -0,0 +1,13 @@
+window.onload = function () {
+    var id = 'mdtext';
+    var el = document.getElementById(id);
+    var text = el.textContent;
+    console.log(text);
+    showdown.setOption('strikethrough', 'true');
+    showdown.setOption('tables', 'true');
+    var converter = new showdown.Converter(),
+        html = converter.makeHtml(text);
+    console.log(html);
+    el.innerHTML = html;
+    //TODO: XSS filtering;
+}
