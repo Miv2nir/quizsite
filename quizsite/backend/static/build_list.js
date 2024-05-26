50afeabba454de6f8b5ab5a2dd9cc522d2d1bdfa
miv2nir <miv2nir@gmail.com>
2024-05-26 13:01:39 +0000

    listing of the answer choices stuff

--- quizsite/backend/static/build_list.js
+++ quizsite/backend/static/build_list.js
@@ -6,15 +6,16 @@ var choices = JSON.parse(thing.textContent);
 console.log(choices);
 var html_text_template = '<div class="content center row">\
 <input type="checkbox" disabled="disabled" class="checkbox-editor">\
-<input id="choice" value="" placeholder="Answer Choice" class="form-field" required id>\
+<input value="" placeholder="Answer Choice" class="form-field" required id>\
 </div>';
-var html_list_items = document.createElement('div');
-html_list_items.innerHTML = html_text_template;
-var html_list_container = document.getElementById('choices-container');
-html_list_container.appendChild(html_list_items);
-console.log(html_list_items);
 for (var i in choices) {
+    var html_list_items = document.createElement('div');
+    html_list_items.innerHTML = html_text_template;
+    html_list_items.id = choices[i].toLowerCase().replace(' ', '-');
     console.log(choices[i]);
+    var html_list_container = document.getElementById('choices-container');
+    html_list_container.appendChild(html_list_items);
+    //console.log(html_list_items);
     //for each, create an html list element with the prompt to enter text
     //on change, update json
     //also need to serialize and package with the form somehow
