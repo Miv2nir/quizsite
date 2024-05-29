6d9fe1ffcf9cae62498171cc684953d216766c2b
miv2nir <miv2nir@gmail.com>
2024-05-29 20:49:35 +0000

    warning placeholder

--- quizsite/backend/static/page_manager_trigger_warn.js
+++ quizsite/backend/static/page_manager_trigger_warn.js
@@ -3,4 +3,8 @@ function del_page_item(item) {
     //2. show the warn main
     //3. pass the page number down to where it belongs
     console.log(item.value);
+    var main1 = document.getElementById('main');
+    main1.style.display = "none";
+    var main2 = document.getElementById('warn');
+    main2 = document.style.display = "inline";
 }
