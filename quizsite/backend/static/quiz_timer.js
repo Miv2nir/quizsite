f82754a50b14f6aa63722557c7b69812c164de1a
miv2nir <miv2nir@gmail.com>
2024-06-01 15:11:26 +0000

    timer saving between reloads

--- quizsite/backend/static/quiz_timer.js
+++ quizsite/backend/static/quiz_timer.js
@@ -1,6 +1,11 @@
+
+var now = new Date().getTime() / 1000;
+var passed_time = Math.floor(now - initial);
 function countdown() {
-    var now = new Date().getTime() / 1000;
-    var passed_time = Math.floor(now - initial);
+    now = new Date().getTime() / 1000;
+    localStorage.setItem('initial', initial);
+    localStorage.setItem('now', now);
+    passed_time = Math.floor(now - initial);
     console.log(passed_time);
     if (passed_time >= wait) {
         console.log(true);
@@ -8,11 +13,23 @@ function countdown() {
     }
 };
 
-var page_next = document.getElementById('page_next_number').innerHTML;
-console.log(page_next);
 
-var initial = new Date().getTime() / 1000;
-var wait = 10;
+var wait = 15;
+
+var page_number = document.getElementById('current_page_number').innerHTML;
+
+
+var page_next = document.getElementById('page_next_number').innerHTML;
+console.log(localStorage.getItem('page_number_old'), page_number);
+if (localStorage.getItem('page_number_old') == page_number) {
+    var initial = localStorage.getItem('initial');
+}
+else {
+    localStorage.removeItem('initial');
+    var initial = new Date().getTime() / 1000;
+}
+localStorage.setItem('page_number_old', page_number);
+
 setInterval(countdown, 1000);
 
 
