ff8f1208ebe379245b24458c8b2ea48f357028cc
miv2nir <miv2nir@gmail.com>
2024-06-01 15:23:28 +0000

    timer showcase & fixes

--- quizsite/backend/static/quiz_timer.js
+++ quizsite/backend/static/quiz_timer.js
@@ -1,3 +1,5 @@
+var timer_target = document.getElementById('timer');
+timer_target.style.display = 'block';
 
 var now = new Date().getTime() / 1000;
 var passed_time = Math.floor(now - initial);
@@ -6,10 +8,11 @@ function countdown() {
     localStorage.setItem('initial', initial);
     localStorage.setItem('now', now);
     passed_time = Math.floor(now - initial);
+    timer_target.innerHTML = 'Time left: ' + (wait - passed_time).toString();
     console.log(passed_time);
     if (passed_time >= wait) {
         console.log(true);
-        window.location.replace('/courses/quiz_test/browse/' + page_next.toString() + '/')
+        window.location.replace(redir_target);
     }
 };
 
@@ -20,6 +23,7 @@ var page_number = document.getElementById('current_page_number').innerHTML;
 
 
 var page_next = document.getElementById('page_next_number').innerHTML;
+var course_name = document.getElementById('current_course_name').innerHTML;
 console.log(localStorage.getItem('page_number_old'), page_number);
 if (localStorage.getItem('page_number_old') == page_number) {
     var initial = localStorage.getItem('initial');
@@ -29,8 +33,14 @@ else {
     var initial = new Date().getTime() / 1000;
 }
 localStorage.setItem('page_number_old', page_number);
+if (page_number == page_next) {
+    var redir_target = '/courses/' + course_name + '/';
+}
+else {
+    var redir_target = '/courses/' + course_name + '/browse/' + page_next.toString() + '/';
+}
 
 setInterval(countdown, 1000);
-
+window.onload = countdown;
 
 
