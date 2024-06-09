4a9efd6e5c92bdd5c55934537f1b0878d59c4ffc
miv2nir <miv2nir@gmail.com>
2024-06-09 11:52:22 +0000

    Assignments: list all public courses so that one could add them to a group by a single click

--- quizsite/backend/static/group_assignments_click.js
+++ quizsite/backend/static/group_assignments_click.js
@@ -1,1 +1,8 @@
-var field = document.getElementById('id_course')
+var field = document.getElementById('id_course');
+console.log(field);
+
+function fill_course_name(item) {
+    var t = item.getElementsByClassName('course-text')[0];
+    console.log(t.innerHTML);
+    field.value = t.innerHTML;
+}
